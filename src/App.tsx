6c267332525ded873928d99355b4a2be82517a5f
Luke Henning-Smith <lukeusydwork@gmail.com>
2026-04-20 22:47:05 +1000

    Remove template code

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
 import Cubes from "./components/Cubes";
+import Clouds from "./components/Clouds";
+import Earth from "./components/Earth";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
       <div
         style={{
           justifyItems: "center",
@@ -38,6 +15,10 @@ function App() {
           style={{ padding: "50px", marginTop: "-50px", marginBottom: "50px" }}
         >
           <Cubes width={300} height={300} />
+
+          <Clouds />
+
+          <Earth />
         </div>
       </div>
     </>
